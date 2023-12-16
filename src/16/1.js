22ca37cb88345b9f843740755f755bc097fc3001
s-kalaus <sergey.kalaus@gmail.com>
2023-12-16 22:58:18 +0100

    16 - refactor

--- src/16/1.js
+++ src/16/1.js
@@ -1,86 +1,55 @@
 import { loadInput } from '../utils/load-input.js';
 
+const progsMirrors = {
+  '/': {
+    E: (beam) => (beam.d = 'N'),
+    N: (beam) => (beam.d = 'E'),
+    W: (beam) => (beam.d = 'S'),
+    S: (beam) => (beam.d = 'W'),
+  },
+  '\\': {
+    E: (beam) => (beam.d = 'S'),
+    N: (beam) => (beam.d = 'W'),
+    W: (beam) => (beam.d = 'N'),
+    S: (beam) => (beam.d = 'E'),
+  },
+};
+
+const progsSPlitters = {
+  '|': {
+    E: (beam, beams) => {
+      beam.d = 'S';
+      beams.push({ x: beam.x, y: beam.y, d: 'N' });
+    },
+    W: (beam, beams) => {
+      beam.d = 'N';
+      beams.push({ x: beam.x, y: beam.y, d: 'S' });
+    },
+  },
+  '-': {
+    N: (beam, beams) => {
+      beam.d = 'E';
+      beams.push({ x: beam.x, y: beam.y, d: 'W' });
+    },
+    S: (beam, beams) => {
+      beam.d = 'W';
+      beams.push({ x: beam.x, y: beam.y, d: 'E' });
+    },
+  },
+};
+
+const progsDir = {
+  E: (beam) => (beam.x += 1),
+  N: (beam) => (beam.y -= 1),
+  W: (beam) => (beam.x -= 1),
+  S: (beam) => (beam.y += 1),
+};
+
 const bootstrap = (input) => {
   const data = input.split('\n').map((line) => line.split(''));
   const visited = {};
   const visitedVectors = {};
   const beams = [{ x: 0, y: 0, d: 'E' }];
-  const progs = {
-    '/': {
-      E: (beam) => {
-        beam.d = 'N';
-        progsDir[beam.d](beam);
-      },
-      N: (beam) => {
-        beam.d = 'E';
-        progsDir[beam.d](beam);
-      },
-      W: (beam) => {
-        beam.d = 'S';
-        progsDir[beam.d](beam);
-      },
-      S: (beam) => {
-        beam.d = 'W';
-        progsDir[beam.d](beam);
-      },
-    },
-    '\\': {
-      E: (beam) => {
-        beam.d = 'S';
-        progsDir[beam.d](beam);
-      },
-      N: (beam) => {
-        beam.d = 'W';
-        progsDir[beam.d](beam);
-      },
-      W: (beam) => {
-        beam.d = 'N';
-        progsDir[beam.d](beam);
-      },
-      S: (beam) => {
-        beam.d = 'E';
-        progsDir[beam.d](beam);
-      },
-    },
-    '|': {
-      E: (beam) => {
-        beam.d = 'S';
-        progsDir[beam.d](beam);
-        beams.push({ x: beam.x, y: beam.y, d: 'N' });
-        const nBeam = beams[beams.length - 1];
-        progsDir[nBeam.d](nBeam);
-      },
-      W: (beam) => {
-        beam.d = 'N';
-        progsDir[beam.d](beam);
-        beams.push({ x: beam.x, y: beam.y, d: 'S' });
-        const nBeam = beams[beams.length - 1];
-        progsDir[nBeam.d](nBeam);
-      },
-    },
-    '-': {
-      N: (beam) => {
-        beam.d = 'E';
-        progsDir[beam.d](beam);
-        beams.push({ x: beam.x, y: beam.y, d: 'W' });
-        const nBeam = beams[beams.length - 1];
-        progsDir[nBeam.d](nBeam);
-      },
-      S: (beam) => {
-        beam.d = 'W';
-        progsDir[beam.d](beam);
-        beams.push({ x: beam.x, y: beam.y, d: 'E' });
-        const nBeam = beams[beams.length - 1];
-        progsDir[nBeam.d](nBeam);
-      },
-    },
-  };
-  const progsDir = {
-    E: (beam) => (beam.x += 1),
-    N: (beam) => (beam.y -= 1),
-    W: (beam) => (beam.x -= 1),
-    S: (beam) => (beam.y += 1),
-  };
 
   while (beams.length) {
     for (const beamIndex in beams) {
@@ -98,11 +67,15 @@ const bootstrap = (input) => {
       visited[key] = true;
       visitedVectors[keyVector] = true;
 
-      if (progs[data[y][x]]?.[d]) {
-        progs[data[y][x]][d](beam);
-      } else {
-        progsDir[d](beam);
+      if (progsMirrors[data[y][x]]?.[d]) {
+        progsMirrors[data[y][x]][d](beam);
+      } else if (progsSPlitters[data[y][x]]?.[d]) {
+        progsSPlitters[data[y][x]][d](beam, beams);
+        const beamAlt = beams[beams.length - 1];
+        progsDir[beamAlt.d](beamAlt);
       }
+
+      progsDir[beam.d](beam);
     }
   }
 
